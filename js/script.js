e2cb5d53965cb226802f49f53628056bf9445d3b
bowman1969 <bowman.design.pro@gmail.com>
2026-03-29 14:40:03 -0400

    Cleaned up all code and added a bit of CSS

--- js/script.js
+++ js/script.js
@@ -10,17 +10,17 @@ let images = [];
 // Function to fetch GIF data from Giphy
 async function getGifs() {
   const response = await fetch(
-    "https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY_HERE&q=dogs&limit=12&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+    "https://api.giphy.com/v1/gifs/search?api_key=9r3pFzDO8hcv09qFzGnQkcfM6d7QYNPQ&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
   );
 
   const data = await response.json();
 
-  // Store all original image URLs in the images array
+  // Store images in the images array
   images = data.data.map(function (gif) {
     return gif.images.original.url;
   });
 
-  // Preview the array in the browser console
+  // Show the images
   console.log(images);
 }
 
@@ -32,7 +32,7 @@ button.addEventListener("click", async function () {
   // Fetch the GIFs first
   await getGifs();
 
-  // Display them on the page
+  // Show them on the page
   for (let i = 0; i < images.length; i++) {
     gifContainer.innerHTML += `<img src="${images[i]}" class="col-3 mb-3">`;
   }
