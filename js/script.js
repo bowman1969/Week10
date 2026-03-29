600b0278f532f520c8af70de954bb1b81c8e737b
bowman1969 <bowman.design.pro@gmail.com>
2026-03-29 13:50:44 -0400

    Setting up structure and API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("script.js loaded");
+
+async function getDogFact() {
+  const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+  const data = await response.json();
+  const fact = data.facts[0];
+  
+  // Update the DOM with the dog fact
+  const output = document.getElementById("dog-fact-output"); 
+  output.textContent = fact;
+}
+
+getDogFact()
+
+// getDogFact() code not shown
+const button = document.getElementById("fetch-dog-btn"); 
+
+button.addEventListener("click", function () { 
+  getDogFact(); 
+});
